0c567d4033cfcccf208d554d184a49d4075f11fb
joo1201 <joo221201@gmail.com>
2023-11-28 16:26:18 +0900

    1차 완료

--- main.js
+++ main.js
@@ -1,22 +1,47 @@
-const headerItem = document.querySelector("header");
-const homeSection = document.querySelector("section.home");
-let homeSectionHeight = homeSection.offsetHeight;
+document.addEventListener("DOMContentLoaded", function() {
+  const headerItem = document.querySelector("header");
+  const homeSection = document.querySelector("#home");
+  let homeSectionHeight = homeSection.offsetHeight;
+  
+  window.addEventListener('resize', function(){
+    homeSectionHeight = homeSection.offsetHeight;
+  })
+  
+  // header
+  window.addEventListener('scroll', function(){
+    if(this.window.scrollY > homeSectionHeight/3){
+      headerItem.style.backgroundColor = '#fff',
+      headerItem.style.width = '100%',
+      headerItem.style.margin = '0'
+      headerItem.style.borderTop = '1px solid #000'
+    }else(
+      headerItem.style.backgroundColor = 'transparent',
+      headerItem.style.width = 'calc(100% - 40px)',
+      headerItem.style.margin = '20px',
+      headerItem.style.borderTop = '2px solid #000'
+    )
+  })
 
-window.addEventListener('resize', function(){
-  homeSectionHeight = homeSection.offsetHeight;
+  // scroll move
+  var menuLinks = document.querySelectorAll("header ul.mega-menu li a");
+
+  menuLinks.forEach(function(link) {
+    link.addEventListener("click", function(e) {
+      
+
+      // 부드럽게 스크롤
+      e.preventDefault();
+      let linkLocation = this.getAttribute('href');
+      var targetElement = document.querySelector(linkLocation);
+      
+      if (linkLocation) {
+        var targetOffsetTop = targetElement.offsetTop;
+        window.scrollTo({
+          top: targetOffsetTop,
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+
 })
-
-window.addEventListener('scroll', function(){
-  if(this.window.scrollY > homeSectionHeight/3){
-    headerItem.style.backgroundColor = '#fff',
-    headerItem.style.width = '100%',
-    headerItem.style.margin = '0'
-    headerItem.style.borderTop = '1px solid #000'
-  }else(
-    headerItem.style.backgroundColor = 'transparent',
-    headerItem.style.width = 'calc(100% - 40px)',
-    headerItem.style.margin = '20px',
-    headerItem.style.borderTop = '2px solid #000'
-  )
-})
-
